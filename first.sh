56d96393db10a978002a70eee399daa36983c993
bhargavi <73773054+bhargavikotaprolu12@users.noreply.github.com>
2023-02-06 22:45:51 +0530

    Add files via upload

---
+++ first.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+echo "i will be a great devops engineer"a
+e="bhargavi"
+echo "hello $e"
+ read age
+echo "my age is $age"
+
+
+echo "sub : hwllo"
+
+sleep 2
+ echo "me : welcome"
+sleep 2
+echo "please sunscribe"
+
+
+
